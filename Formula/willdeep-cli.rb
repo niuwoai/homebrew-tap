56e0b46927aec976e69ae17dfcdac5ed0319f4f1
rocky <m@some.im>
2026-09-21 15:41:23 +0800

    chore: normalize formula metadata

--- Formula/willdeep-cli.rb
+++ Formula/willdeep-cli.rb
@@ -1,11 +1,10 @@
 class WilldeepCli < Formula
   desc "Command-line client and runtime tools for WillDeep"
   homepage "https://github.com/niuwoai/willdeep-rs"
-  version "0.79.0-rc1"
-  license "MIT"
-
   url "https://github.com/niuwoai/willdeep-rs/releases/download/v0.79.0-rc1/willdeep-macos-universal.tar.gz"
+  version "0.79.0-rc1"
   sha256 "95d773579de21fae8abe8498cee7aacaccf2591b46a315a1f9a0891e4b6758c5"
+  license "MIT"
 
   def install
     bin.install "willdeep"
