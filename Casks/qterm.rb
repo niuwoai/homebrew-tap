02e1980964abb0bdf54d2abafc0830837eb9760c
rocky <m@some.im>
2026-09-24 01:51:38 +0800

    qterm 1.244.0-rc2

--- Casks/qterm.rb
+++ Casks/qterm.rb
@@ -1,6 +1,6 @@
 cask "qterm" do
-  version "1.243.0-rc6"
-  sha256 "48444a5c5f482004dbe50ed07e1e0139c90d1de38089f956cecff25cd7231235"
+  version "1.244.0-rc2"
+  sha256 "c558cbe650aa17ecb5bb8b3e460a59ce577271a3d86ccf1639b44eb81948744f"
 
   url "https://img.niuwoai.com/mac-apps/QTerm-#{version}.dmg"
   name "QTerm"
