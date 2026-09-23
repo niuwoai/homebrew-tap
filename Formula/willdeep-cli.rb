ad77b58ebe5b8e7522501717e9ed84508de13efc
rocky <m@some.im>
2026-09-23 16:53:41 +0800

    willdeep-cli 0.81.0-rc8

--- Formula/willdeep-cli.rb
+++ Formula/willdeep-cli.rb
@@ -1,9 +1,9 @@
 class WilldeepCli < Formula
   desc "Command-line client and runtime tools for WillDeep"
   homepage "https://github.com/niuwoai/willdeep-rs"
-  url "https://github.com/niuwoai/willdeep-rs/releases/download/v0.81.0-rc7/willdeep-macos-universal.tar.gz"
-  version "0.81.0-rc7"
-  sha256 "43b1736cb779c40d6f8a192afbce50fb252672856262eb4948383ce6984a85f3"
+  url "https://github.com/niuwoai/willdeep-rs/releases/download/v0.81.0-rc8/willdeep-macos-universal.tar.gz"
+  version "0.81.0-rc8"
+  sha256 "c491821f7534fbf2fe95c7855db30944dbd9f41c671bae354965b24ba508bc4b"
   license "MIT"
 
   def install
